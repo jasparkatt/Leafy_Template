9c3304e7b5e6207ac80c68ea62f3ee15cb3431db
jasparkatt <jasparkatt@yahoo.com>
2019-05-05 14:05:35 -0500

    1st upload of file and folder structure

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+"use strict";
+//load plugins
+const gulp = require("gulp");
+const newer = require("gulp-newer");
+const htmlclean = require("gulp-htmlclean");
+const del = require("del");
+const rename = require("gulp-rename");
