64b4f54a46af04c1c1d05aecaa1eb1679af31d26
jasparkatt <jasparkatt@yahoo.com>
2019-05-11 07:17:11 -0500

    working on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 "use strict";
 //load plugins
-const gulp = require("gulp");
-const newer = require("gulp-newer");
-const htmlclean = require("gulp-htmlclean");
-const del = require("del");
-const rename = require("gulp-rename");
+
+const {src, dest, watch, series, parallel} = require('gulp');
+const newer = require("gulp-newer");//pass only source files that //are newer than corresponding destination files
+const del = require("del");//clean out folders
+const rename = require("gulp-rename");//rename files
+const plumber = require("gulp-plumber"); //prevent pipe breaking
+const cleanCss = require("gulp-clean-css");//minify css
+const htmlmin = require("gulp-htmlmin");//minify html
+const uglify = require("gulp-uglify");//minify js
+
