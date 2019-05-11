c4801e0ef13fb14df0b765eb5f1d8059f03896bc
jasparkatt <jasparkatt@yahoo.com>
2019-05-11 08:19:41 -0500

    start building. Update gulpfile with css task, create dist dir and subs/min files

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,27 @@
 //load plugins
 
 const {src, dest, watch, series, parallel} = require('gulp');
-const newer = require("gulp-newer");//pass only source files that //are newer than corresponding destination files
+const newer = require("gulp-newer");//pass only source files that
+//are newer than corresponding destination files
 const del = require("del");//clean out folders
 const rename = require("gulp-rename");//rename files
 const plumber = require("gulp-plumber"); //prevent pipe breaking
 const cleanCss = require("gulp-clean-css");//minify css
 const htmlmin = require("gulp-htmlmin");//minify html
 const uglify = require("gulp-uglify");//minify js
+
+//construct some file paths
+const files = {
+    jsPath: 'src/scripts/*.js',
+    cssPath: 'src/style/*.css'
+}
+//css task
+function cssTask(){
+    return src(files.cssPath)
+    .pipe(plumber())
+    .pipe(cleanCss())
+    .pipe(rename('main.min.css'))
+    .pipe(dest('dist/style'));
+}
+exports.default = cssTask
 
