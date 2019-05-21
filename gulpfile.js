9b86e67cc6395600d1e4122e71ab235f9b3ae2a1
Jon Galloy <jon.galloy@Waupaca-Cty.local>
2019-05-21 07:38:11 -0500

    fix devbuild task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 "use strict";
 //load plugins
 
-const {src, dest, watch, series, parallel} = require('gulp');
-const newer = require("gulp-newer");//pass only source files that
+const { src, dest, watch, series, parallel } = require('gulp');
+const newer = require("gulp-newer"); //pass only source files that
 //are newer than corresponding destination files
-const del = require("del");//clean out folders
-const rename = require("gulp-rename");//rename files
+const del = require("del"); //clean out folders
+const rename = require("gulp-rename"); //rename files
 const plumber = require("gulp-plumber"); //prevent pipe breaking
-const cleanCss = require("gulp-clean-css");//minify css
-const htmlmin = require("gulp-htmlmin");//minify html
-const uglify = require("gulp-uglify");//minify js
+const cleanCss = require("gulp-clean-css"); //minify css
+const htmlmin = require("gulp-htmlmin"); //minify html
+const uglify = require("gulp-uglify"); //minify js
 const copyNodeModules = require("copy-node-modules");
 const shell = require("gulp-shell");
 const concat = require("gulp-concat");
@@ -22,110 +22,114 @@ const browserSync = require('browser-sync');
 
 //construct some file paths
 const files = {
-    buildPath: ['src/scripts/main.js', 'src/style/main.css'],
-    imgPath: 'src/img/*.png',
-    coreCssPath: 'src/core_scripts/*.css',//may need to explicity set order if this doesnt work?
-    coreJsPath: 'src/core_scripts/*.js',//ditto ^^
-    jsPath: 'src/scripts/*.js',
-    cssPath: 'src/style/*.css',
-    htmlPath: 'src/index.html',
-    nodePath: './',
-    stylesBuild: './dist/styles/', // this is where the minified, compiled css will go
-    jsBuild: './dist/scripts/',//this is where the minified js will go
-    nodeBuild: '.dist/',
-    coreBuild: './dist/core_scripts/',//this is where the plugin js and css will go after being min and conct
-    sourcemapsBuild: '../',
-    imgBuild: './dist/img/',
-    deletedPaths: './dist',//delete the dist dir before a new build
-    /* conJsmain: './dist/scripts/main.min.js',
-    conJscore: './dist/core_scripts/core.js',
-    conCssmain: './dist/styles/main.min.css',
-    conCsscore: './dist/core_scripts/core.css' */
-}
-//create our dev server
+        buildPath: ['src/scripts/main.js', 'src/style/main.css'],
+        imgPath: 'src/img/*.png',
+        coreCssPath: 'src/core_scripts/*.css', //may need to explicity set order if this doesnt work?
+        coreJsPath: 'src/core_scripts/*.js', //ditto ^^
+        jsPath: 'src/scripts/*.js',
+        cssPath: 'src/style/*.css',
+        htmlPath: 'src/index.html',
+        nodePath: './',
+        stylesBuild: './dist/styles/', // this is where the minified, compiled css will go
+        jsBuild: './dist/scripts/', //this is where the minified js will go
+        nodeBuild: '.dist/',
+        coreBuild: './dist/core_scripts/', //this is where the plugin js and css will go after being min and conct
+        sourcemapsBuild: '../',
+        imgBuild: './dist/img/',
+        deletedPaths: './dist', //delete the dist dir before a new build
+        /* conJsmain: './dist/scripts/main.min.js',
+        conJscore: './dist/core_scripts/core.js',
+        conCssmain: './dist/styles/main.min.css',
+        conCsscore: './dist/core_scripts/core.css' */
+    }
+    //create our dev server
 function serve() {
-  return browserSync.init({
-    server: './',
-    open: true,
-    port: 3000
-  });
+    return browserSync.init({
+        server: './',
+        open: true,
+        port: 3000
+    });
 }
 //clean cache for live reload issues....
+//need to change the src files so it doensnt change the index.html file in root on task run
 var cbString = new Date().getTime();
-function cacheBustTask(){
+
+function cacheBustTask() {
     return src(files.htmlPath)
         .pipe(replace(/cb=\d+/, 'cb=' + cbString))
         .pipe(dest('.'));
 }
 //clean out build folder i.e dist
-function clean(){
+function clean() {
     return del([files.deletedPaths]);
     console.log('Files and folders that would be deleted:\n', files.deletedPaths.join('\n'));
 }
 //need task to copy over faviconslook at buffer
-function copyFav(){
+function copyFav() {
     return src('./favicon.ico')
-    .pipe(copy('dist',{prefix:1}))
-    .pipe(dest('./'));
+        .pipe(copy('dist', { prefix: 1 }))
+        .pipe(dest('./'));
 }
 //need a minify image task for pngs
-function imgTask(){
+function imgTask() {
     return src(files.imgPath)
-    .pipe(plumber())
-    .pipe(imagemin())
-    .pipe(dest(files.imgBuild))
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(dest(files.imgBuild))
 }
 //need a minify the css and html after concat too
 
 //css task
-function cssTask(){
+function cssTask() {
     return src(files.cssPath)
-    .pipe(plumber())
-    .pipe(cleanCss())
-    .pipe(rename('main.min.css'))
-    .pipe(dest(files.stylesBuild))
+        .pipe(plumber())
+        .pipe(cleanCss())
+        .pipe(rename('main.min.css'))
+        .pipe(dest(files.stylesBuild))
 }
 //js task
-function jsTask(){
+function jsTask() {
     return src(files.jsPath)
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(rename('main.min.js'))
-    .pipe(dest(files.jsBuild))
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(dest(files.jsBuild))
 }
 //html tasks
-function htmlTask(){
+function htmlTask() {
     return src(files.htmlPath)
-    .pipe(plumber())
-    .pipe(htmlmin({removeComments: true}))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(rename('index.min.html'))
-    .pipe(dest(files.deletedPaths))
+        .pipe(plumber())
+        .pipe(htmlmin({ removeComments: true }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(rename('index.min.html'))
+        .pipe(dest(files.deletedPaths))
 }
 //copy and minify and concat the js and css
 //found in src core_scripts. these are standard
 //leaflet plugins
 //core scripts task
-function coreCssTask(){
+function coreCssTask() {
     return src(files.coreCssPath)
-    .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(cleanCss('*.css'))
-        .pipe(concat('core.css'))        
-    .pipe(sourcemaps.write(files.sourcemapsBuild))
-    .pipe(dest(files.coreBuild))
+        .pipe(concat('core.css'))
+        .pipe(sourcemaps.write(files.sourcemapsBuild))
+        .pipe(dest(files.coreBuild))
 }
-function coreJsTask(){
+
+function coreJsTask() {
     return src(files.coreJsPath)
-    .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(uglify('*.js'))
         .pipe(concat('core.js'))
-    .pipe(sourcemaps.write(files.sourcemapsBuild))
-    .pipe(dest(files.coreBuild))
+        .pipe(sourcemaps.write(files.sourcemapsBuild))
+        .pipe(dest(files.coreBuild))
 }
+
 function devBuild() {
     return src(files.buildPath)
-    .pipe(copy('temp'))
-    .pipe(dest('./'));
+        .pipe(copy('temp'))
+        .pipe(dest('./'));
 }
 
 /* function concatjsTask(){
@@ -142,15 +146,15 @@ function concatCssTask(){
 //copy needed packages from node folder i.e leaflet
 //need to tweek code to just keep the js/css you need
 //can probably do something similar to this for plugins folder copyover
-function nodeTask(){
+function nodeTask() {
     return src(files.nodePath)
-    .pipe(shell('node copyNode'))
+        .pipe(shell('node copyNode'))
 }
 //create some watch tasks
-function watchTask(){
+function watchTask() {
     watch([files.cssPath, files.jsPath, files.htmlPath]),
         cacheBustTask,
-        parallel(cssTask,jsTask,htmlTask),
+        parallel(cssTask, jsTask, htmlTask),
         parallel(coreCssTask, coreJsTask),
         serve
 }
@@ -174,26 +178,26 @@ exports.concatCssTask = concatCssTask; */
 exports.default = series(
     clean,
     nodeTask,
-    parallel(imgTask,copyFav, cssTask, jsTask, htmlTask, coreCssTask, coreJsTask)
+    parallel(imgTask, copyFav, cssTask, jsTask, htmlTask, coreCssTask, coreJsTask)
 );
 exports.cleanCor = series(
     cacheBustTask,
     clean,
     nodeTask,
     parallel(cssTask, jsTask, htmlTask),
-    parallel(imgTask,copyFav),
+    parallel(imgTask, copyFav),
     parallel(coreCssTask, coreJsTask)
     // parallel(concatjsTask, concatCssTask)
 );
 exports.rebuild = series(
-cacheBustTask,
-parallel(cssTask, jsTask, htmlTask),
-serve
+    cacheBustTask,
+    parallel(cssTask, jsTask, htmlTask),
+    serve
 );
 exports.devtst = series(
-cacheBustTask,
-devBuild,
-serve);
+    cacheBustTask,
+    devBuild,
+    serve);
 //need to figure out task to use jsut the html from ./
 //to run against the css and js in the src folder.
 //and have that particular build be used by server
